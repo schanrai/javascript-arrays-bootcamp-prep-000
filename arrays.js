e0b0a6ab32896293dfee047e9c22d2f05ac5af6e
Sushi Chanrai <sushi@pathworks.co>
2018-05-23 18:42:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,5 +36,11 @@ function accessElementInArray(array,i){
     array.pop();
     return array;
   }
+  
+  function removeElementFromEndOfArray(array){
+  array.slice(0, array.length-1);
+  return array;
+ }
+  
  
   
